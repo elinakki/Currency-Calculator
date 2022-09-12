270eef7db9f8fa049b1f4b99bdcaec7453768095
elinakki <99351227+elinakki@users.noreply.github.com>
2022-09-12 18:19:48 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react'
+
+function App() {
+  const [eur, setEur] = useState(0)
+  const [gbp, setGbp] = useState(0)
+
+  function calculate(e) {
+    e.preventDefault();
+    const conversion = eur * 0.85
+    setGbp(conversion)
+  }
+
+  return (
+    <form onSubmit={calculate}>
+      <h3>Currency Calculate</h3>
+      <div>
+        <label>EUR</label>
+        <input type="number" value={eur} onChange={e => setEur(e.target.value)}/>
+      </div>
+      <div>
+        <label>GPB</label>
+        <output>{gbp}</output>
+      </div>
+      <button>Calculate</button>
+    </form>
+  );
+}
+
+export default App;
